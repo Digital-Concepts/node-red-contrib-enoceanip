deba641c357deaa927903b5f9b3dbaf7f27a2934
Dominic Böhler <dominic.boehler@digital-concepts.eu>
2016-02-27 14:01:38 +0100

    Refactored project
    - Renamed files
    - global configuration list of gateways connections
    - 3 nodes in enocean suite (separation of concerns)

---
+++ enocean/apiconnection.js
@@ -0,0 +1,207 @@
+'use strict';
+
+// DCGW uses self signed certificates
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var EventEmitter = require('events').EventEmitter;
+var request = require('request');
+
+var APIConnection = module.exports = function APIConnection(mconfig) {
+  EventEmitter.call(this);
+  var config = mconfig;
+
+  console.log("APIConnection()" + mconfig);
+  console.log("APIConnection().host:" + mconfig.host);
+
+  this.getConfig = function() {
+        console.log("getConfig" + JSON.stringify(config));
+
+        return config;
+  };
+
+  return this;
+};
+
+APIConnection.prototype = Object.create(EventEmitter.prototype);
+APIConnection.API_STREAM     = '/devices/stream?delimited=newLine&output=singleLine';
+APIConnection.API_SYSTEMINFO = '/systemInfo';
+APIConnection.API_DEVICES    = '/devices';
+APIConnection.API_PROFILES   = '/profiles';
+
+APIConnection.prototype.doRequest = function doRequest(endpoint){
+  var self = this;
+  var url = this.getConfig().host + ':' + this.getConfig().port;
+
+  switch(endpoint.toLowerCase()){
+    case 'devices':
+      url += APIConnection.API_DEVICES;
+      break;
+    case 'profiles':
+      url += APIConnection.API_PROFILES;
+      break;
+    case 'systeminfo':
+      url += APIConnection.API_SYSTEMINFO;
+      break;
+  }
+
+  try {
+      request({   
+          url: url, 
+          method: 'GET'
+      },  
+      function (error, response, body) {
+
+          if (!error && response.statusCode === 200) {
+
+              self.emit('getanswer', body);
+
+          } else {
+              self.emit('error', 'EnOcean API Input Error', {'error0x01' : body});
+          }
+      }).auth(this.getConfig().user, this.getConfig().password, false);
+  } catch (e){
+     self.emit('error','HTTP error', { "error0xFF" : "unknown" });
+  }
+};
+
+APIConnection.prototype.putRequest = function putRequest(msg){
+    var self = this;
+    try {
+  
+      var deviceId = msg.payload.deviceId;
+      delete msg.payload.deviceId;
+
+
+
+      request({   
+          url: this.getConfig().host + ':' + this.getConfig().port + '/devices/' + deviceId + '/state', 
+          method: 'PUT', 
+          json: msg.payload
+      },  
+      function (error, response, body) {
+          var msg = {};
+          msg.body = body;
+
+          if (!error && response.statusCode === 200) {
+            // HOW TO HANDLE GOOD FEEDBACK ?
+          } else {
+              self.emit('error', 'EnOcean API Input Error', {'error0x01' : body});
+          }
+      }).auth(this.getConfig().user, this.getConfig().password, false);
+    } catch (e){
+        console.log(JSON.stringify(e));
+        this.emit('error','HTTP error', { "error0xFF" : "unknown" });
+    }
+};
+
+APIConnection.prototype.startstream = function startstream(filter) {
+  var self = this;
+  self.initStage = 1;
+  self.chunks = '';
+
+  console.log("startstream:this.getConfig().host:" + this.getConfig().host);
+
+  var options = {
+    method: 'GET',
+    uri: this.getConfig().host + ':' + this.getConfig().port + APIConnection.API_STREAM + '&direction=' + filter.direction,
+    rejectUnauthorized : false,
+    headers: {
+      accept: '*/*'
+    }
+  };
+
+  console.log(JSON.stringify(options));
+
+  //uri: 'http://172.28.28.150:8080/devices/stream?delimited=newLine&output=singleLine'
+  var stream = request(options, function (error, response, body) {
+      // body is the decompressed response body 
+      if(!error && response !== undefined && response.statusCode === 200) {
+        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'));
+        console.log('the decoded data is: ' + body);
+        console.log(response);
+      } else {
+        self.emit('error','Streaming error', { "error0x05" : error });
+      }
+  }).auth(this.getConfig().user, this.getConfig().password, false).on('data', function(data) {
+      self.chunks += data.toString('utf8');
+
+      // If true when packet has been received in chunks. (wait for nth packet in order to complete)
+      if(self.chunks.indexOf('\r\n')===-1){
+        return;
+      }
+
+      /* Stages:
+      1 - while states not finished
+      0 - while telegram not finished
+      */
+      switch(self.initStage){
+          case 1: // Send states information
+              self.initStage--;
+
+              try{
+                  self.emit('states', JSON.parse(self.chunks));
+              }catch(e){
+                  self.emit('error','Error while parsing JSON in STAGE1, state transmission');
+              }
+              break;
+          case 0: // Send telegram information (without states)
+
+              try{
+                  self.emit('telegram', JSON.parse(self.chunks));
+              }catch(e){
+                  self.emit('error','Error while parsing JSON in STAGE0, telegram transmission');
+              }
+              break;
+          default:
+              break;
+      }
+
+      // get ready for next chunked transmission
+      self.chunks = '';
+  });
+
+  stream.on('response', function(response) {
+      // executed once
+      self.emit('connected');
+
+      /*
+      response.on('data', function(data) {
+          // Also chunks received in here ?
+      });
+      */
+  });
+  
+  stream.on('error', function(err) {
+      console.log(err);
+      this.emit('error','Stream error', { "error0x06" : err });
+  });
+
+
+  stream.on("close", function() {
+      // TODO: implement close function.
+  });
+
+  stream.on("end", function() {
+      // won't happen - neverending stream
+  });
+};
+
+
+/*//local test
+var streamer = new DCStream();
+streamer.start({  'password'  : 'user',
+                  'user'      : 'user',
+                  'host'      : 'https://172.28.28.150',
+                  'port'      : '8080'
+});
+
+streamer.on('telegram', function(msg) {
+  console.log("telegram:" + msg);
+});
+
+
+
+streamer.on('states', function(msg) {
+  console.log("states:" + msg);
+});
+*/
---
+++ enocean/enoceansuite.js
@@ -0,0 +1,167 @@
+'use strict';
+
+var APIConnection = require('./apiconnection.js');
+
+module.exports = function(RED) {
+
+    function EnOceanGW(config){
+        RED.nodes.createNode(this, config);     
+
+        this.host = config.host;
+        this.port = config.port;
+        this.user = config.user;
+        this.password = config.password;
+        this.direction = config.direction;
+    }
+    RED.nodes.registerType("enocean gw",EnOceanGW);
+
+
+    // Send state node
+    function EnOceanOutNode(config){
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        // Retrieve the configuration gw node
+        this.gw = RED.nodes.getNode(config.gw);
+
+        var gwcon = new APIConnection(this.gw);
+        this.config = config;
+
+        gwcon.on('error', function(e){
+            node.error("Network error: " + JSON.stringify(e));
+            //node.status({fill: 'red', shape: "ring", text: "disconnected"});
+        });
+
+        /*NODE-RED events*/
+        this.on('input', function(msg) {     
+
+            // Check type and convert if neccessary
+            if (typeof msg === 'string' || msg instanceof String){
+                try {
+                    msg = JSON.parse(msg);
+                } catch(e) {
+                    node.error('API Input Error: The input has no JSON format');
+                    return;
+                }
+            }
+
+            // simple validity check
+            if( msg.payload.deviceId === undefined ||
+                msg.payload.state === undefined   ||
+                msg.payload.state.functions === undefined
+                ){
+                node.error('API Input Error', {'error0x02' : 'API Syntax: msg.payload = {  "deviceId":"01870183", "state":{  "functions":[ {   "key":"dimValue",  "value":"1.0" } ] } }'});
+            } else {
+                gwcon.putRequest(msg);
+            }
+        });
+    }
+    RED.nodes.registerType("enocean out",EnOceanOutNode);
+
+    // Streaming API node
+    function EnOceanInNode(config) {
+        RED.nodes.createNode(this, config);
+
+        // Retrieve the configuration gw node
+        this.gw = RED.nodes.getNode(config.gw);
+
+
+        var node = this;
+        var gwcon = new APIConnection(this.gw);
+        this.config = config;
+
+        gwcon.on('getanswer', function(answer){
+            node.send(answer);
+        });
+
+        this.on('input', function(msg) {  
+            if(msg.payload.get !== undefined){
+                switch(msg.payload.get.toLowerCase()){
+                    case 'devices':
+                    case 'profiles':
+                    case 'systemInfo':
+                        gwcon.getRequest(msg.payload.get);
+                        break;
+                    default:
+                        node.error('API Input Error', {'error0x03':'API Syntax: msg.payload = { "get" : "COMMAND_NAME"}'});
+                        break;
+                }   
+            }
+        });
+
+        gwcon.on('connected', function(){
+            node.status({fill: 'green', shape: "ring", text: "connected"});
+        });
+
+        gwcon.on('states', function(json){
+            if(node.config.statesflag){
+                var msg = {};
+                msg.payload = json;
+                node.send(msg);
+            }
+        });      
+
+        gwcon.on('telegram', function(json){
+            var msg = {};
+            msg.payload = json;
+            node.send(msg);
+        }); 
+
+        gwcon.on('error', function(e){
+            node.error("Network error: " + e);
+            node.status({fill: 'red', shape: "ring", text: "disconnected"});
+        });
+
+        node.status({fill: 'yellow', shape: "ring", text: "connecting"});
+
+        /*NODE-RED events*/
+        this.on('close', function() {
+            // TODO: implement close function
+        });
+
+        var filter = { direction: config.direction};
+        gwcon.startstream(filter);
+    }
+    RED.nodes.registerType("enocean in",EnOceanInNode);
+
+    // Function gw request state node
+    function EnOceanGWRequest(config){
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        // Retrieve the configuration gw node
+        this.gw = RED.nodes.getNode(config.gw);
+
+        var gwcon = new APIConnection(this.gw);
+        this.config = config;
+
+        gwcon.on('error', function(e){
+            node.error("Network error: " + JSON.stringify(e));
+        });
+
+        /*NODE-RED events*/
+        this.on('input', function(msg) {     
+
+            // Check type and convert if neccessary
+            if (typeof msg === 'string' || msg instanceof String){
+                try {
+                    msg = JSON.parse(msg);
+                } catch(e) {
+                    node.error('API Input Error: The input has no JSON format');
+                    return;
+                }
+            }
+
+            // simple validity check
+            if( msg.payload.deviceId === undefined ||
+                msg.payload.state === undefined   ||
+                msg.payload.state.functions === undefined
+                ){
+                node.error('API Input Error', {'error0x02' : 'API Syntax: msg.payload = {  "deviceId":"01870183", "state":{  "functions":[ {   "key":"dimValue",  "value":"1.0" } ] } }'});
+            } else {
+                gwcon.putRequest(msg);
+            }
+        });
+    }
+    RED.nodes.registerType("enocean gwrequest",EnOceanGWRequest);
+};
