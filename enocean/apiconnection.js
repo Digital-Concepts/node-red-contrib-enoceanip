4a39f5f22cecf8630d04337007115182fdc04f8f
Dominic Böhler <dominic.boehler@digital-concepts.eu>
2016-03-08 13:39:06 +0100

    Updated help texts and ran jsbeautifier

--- enocean/apiconnection.js
+++ enocean/apiconnection.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 // Allow HTTPS communication with servers that use self signed certificates
@@ -7,177 +8,173 @@ var EventEmitter = require('events').EventEmitter;
 var request = require('request');
 
 var APIConnection = module.exports = function APIConnection(mconfig, mcredentials) {
-  EventEmitter.call(this);
-  var config = mconfig;
-  var credentials = mcredentials;
-  var stream;
+    EventEmitter.call(this);
+    var config = mconfig;
+    var credentials = mcredentials;
+    var stream;
 
-  this.getBaseURL = function() {
-      return config.host + ':' + config.port;
-  };
+    this.getBaseURL = function() {
+        return config.host + ':' + config.port;
+    };
 
-  this.getCredentials = function() {
-      return credentials;
-  };
+    this.getCredentials = function() {
+        return credentials;
+    };
 
-  this.getStream = function() {
-      return stream;
-  };
+    this.getStream = function() {
+        return stream;
+    };
 
-  this.setStream = function(pstream) {
-      stream = pstream;
-  };
+    this.setStream = function(pstream) {
+        stream = pstream;
+    };
 
-  return this;
+    return this;
 };
 
 APIConnection.prototype = Object.create(EventEmitter.prototype);
-APIConnection.API_STREAM     = '/devices/stream?delimited=newLine&output=singleLine';
-APIConnection.API_PROFILES   = 'profiles';
-APIConnection.API_STATES     = 'states';
+APIConnection.API_STREAM = '/devices/stream?delimited=newLine&output=singleLine';
+APIConnection.API_PROFILES = 'profiles';
+APIConnection.API_STATES = 'states';
 
 
-APIConnection.prototype.apiHandler = function apiHandler(msg){
-  
-  if(msg.items !== undefined){
-    // to deal with N-Requests
-    var self = this;
+APIConnection.prototype.apiHandler = function apiHandler(msg) {
 
-      msg.items.forEach(function(entry) {
-        var path;
+    if (msg.items !== undefined) {
+        // to deal with N-Requests
+        var self = this;
 
-        // Construct URL path
-        if(msg.ressource===APIConnection.API_STATES) {
-          path = '/devices/' + entry + '/state';
-        }
-        else if(msg.ressource===APIConnection.API_PROFILES) {
-          path = '/devices/' + entry + '/profile';
-        }
-        else {
-          path = '/' + msg.ressource + '/' + entry; 
-        }
+        msg.items.forEach(function(entry) {
+            var path;
 
-        // Check which HTTP method needs to be applied 
-        if(msg.state === undefined) {
-          self.doRequest(path, 'GET');
-        } else {
-          self.doRequest(path, 'PUT', { 'state' : msg.state });
-        }
-      });
-  } else {
-    // Single request
-    this.doRequest('/' + msg.ressource, 'GET');
-  }
+            // Construct URL path
+            if (msg.ressource === APIConnection.API_STATES) {
+                path = '/devices/' + entry + '/state';
+            } else if (msg.ressource === APIConnection.API_PROFILES) {
+                path = '/devices/' + entry + '/profile';
+            } else {
+                path = '/' + msg.ressource + '/' + entry;
+            }
+
+            // Check which HTTP method needs to be applied 
+            if (msg.state === undefined) {
+                self.doRequest(path, 'GET');
+            } else {
+                self.doRequest(path, 'PUT', {
+                    'state': msg.state
+                });
+            }
+        });
+    } else {
+        // Single request
+        this.doRequest('/' + msg.ressource, 'GET');
+    }
 };
 
-APIConnection.prototype.doRequest = function doRequest(path, method, payload){
-  var self = this;
-  var url = this.getBaseURL() + path;
+APIConnection.prototype.doRequest = function doRequest(path, method, payload) {
+    var self = this;
+    var url = this.getBaseURL() + path;
 
-  try {
-      request({   
-          url: url, 
-          method: method,
-          body: JSON.stringify(payload),
-          headers: {
-            accept: '*/*'
-          }
-      },  
-      function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-              self.emit('getanswer', JSON.parse(body));
-          } else {
-              self.emit('error', body);
-          }
-      }).auth(this.getCredentials().username, this.getCredentials().password, false);
-  } catch (e){
-     self.emit('error','Unknown HTTP error');
-  }
+    try {
+        request({
+                url: url,
+                method: method,
+                body: JSON.stringify(payload),
+                headers: {
+                    accept: '*/*'
+                }
+            },
+            function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    self.emit('getanswer', JSON.parse(body));
+                } else {
+                    self.emit('error', body);
+                }
+            }).auth(this.getCredentials().username, this.getCredentials().password, false);
+    } catch (e) {
+        self.emit('error', 'Unknown HTTP error: ' + e);
+    }
 };
 
 APIConnection.prototype.startstream = function startstream(filter) {
-  var self = this;
-  self.initStage = 1;
-  self.chunks = '';
-  
-  var options = {
-    method: 'GET',
-    uri: self.getBaseURL() + APIConnection.API_STREAM + '&direction=' + filter.direction,
-    rejectUnauthorized : false,
-    headers: {
-      accept: '*/*'
-    }
-  };
+    var self = this;
+    self.initStage = 1;
+    self.chunks = '';
 
-  self.stream = request(options, function (error, response, body) {
-      // body is the decompressed response body 
-      if(!error && response !== undefined && response.statusCode === 200) {
-        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'));
-        console.log('the decoded data is: ' + body);
-        console.log(response);
-      } else {
-        self.emit("Streaming error" + error);
-      }
-  }).auth(this.getCredentials().username, this.getCredentials().password, false).on('data', function(data) {
-      self.chunks += data.toString('utf8');
+    var options = {
+        method: 'GET',
+        uri: self.getBaseURL() + APIConnection.API_STREAM + '&direction=' + filter.direction,
+        rejectUnauthorized: false,
+        headers: {
+            accept: '*/*'
+        }
+    };
 
-      // If true when packet has been received in chunks. (wait for nth packet in order to complete)
-      if(self.chunks.indexOf('\r\n')===-1){
-        return;
-      }
+    self.stream = request(options, function(error, response, body) {
+        if (!error && response !== undefined && response.statusCode === 200) {
+            // successfull connected to stream
+        } else {
+            self.emit('error', "Streaming error: " + error);
+        }
+    }).auth(this.getCredentials().username, this.getCredentials().password, false).on('data', function(data) {
+        self.chunks += data.toString('utf8');
 
-      /* Stages:
-      1 - while states not finished
-      0 - while telegram not finished
-      */
-      switch(self.initStage){
-          case 1: // Send states information
-              self.initStage--;
+        // If true when packet has been received in chunks. (wait for nth packet in order to complete)
+        if (self.chunks.indexOf('\r\n') === -1) {
+            return;
+        }
 
-              try{
-                  self.emit('states', JSON.parse(self.chunks));
-              }catch(e){
-                  self.emit('error','Error while parsing JSON in state transmission');
-              }
-              break;
-          case 0: // Send telegram information (without states)
+        /* Stages:
+        1 - while states not finished
+        0 - while telegram not finished
+        */
+        switch (self.initStage) {
+            case 1: // Send states information
+                self.initStage--;
 
-              try{
-                  self.emit('telegram', JSON.parse(self.chunks));
-              }catch(e){
-                  self.emit('error','Error while parsing JSON in telegram transmission');
-              }
-              break;
-          default:
-              break;
-      }
+                try {
+                    self.emit('states', JSON.parse(self.chunks));
+                } catch (e) {
+                    self.emit('error', 'Error while parsing JSON in state transmission');
+                }
+                break;
+            case 0: // Send telegram information (without states)
 
-      // get ready for next chunked transmission
-      self.chunks = '';
-  });
+                try {
+                    self.emit('telegram', JSON.parse(self.chunks));
+                } catch (e) {
+                    self.emit('error', 'Error while parsing JSON in telegram transmission');
+                }
+                break;
+            default:
+                break;
+        }
 
-  self.stream.on('response', function(response) {
-      // executed once
-      self.emit('connected');
+        // get ready for next chunked transmission
+        self.chunks = '';
+    });
 
-      /*
-      response.on('data', function(data) {
-          // Also chunks received in here ?
-      });
-      */
-  });
-  
-  self.stream.on('error', function(err) {
-    console.log("stream error event");
-    this.emit('error','Stream error', { "error0x06" : err });
-  });
+    self.stream.on('response', function(response) {
+        // executed once
+        self.emit('connected');
 
-  this.setStream(self.stream);
+        /*
+        response.on('data', function(data) {
+            // Also chunks received in here ?
+        });
+        */
+    });
+
+    self.stream.on('error', function(err) {
+        this.emit('error', 'Streaming error: ' + err);
+    });
+
+    this.setStream(self.stream);
 };
 
-APIConnection.prototype.closeStream = function closeStream(){
-      if(this.getStream()) {
+APIConnection.prototype.closeStream = function closeStream() {
+    if (this.getStream()) {
         this.getStream().pause();
         this.getStream().end();
-      }
+    }
 };
--- enocean/enoceansuite.js
+++ enocean/enoceansuite.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 var createSchema = require('json-gate').createSchema;
@@ -8,85 +9,108 @@ var apiSchema = createSchema(require('./ressources/apischema.json'));
 module.exports = function(RED) {
 
     // Configuration node
-    function EnOceanGW(n){
-        RED.nodes.createNode(this, n);     
+    function EnOceanGW(n) {
+        RED.nodes.createNode(this, n);
         var self = this;
         var gwcon = new APIConnection(n, this.credentials);
         this.config = n;
 
-        // This endpoint is needed to bypass cross domain protection
-        // No Ajax calls allowed from .html / .js, because of different origin.
-        RED.httpAdmin.get('/devices', function(req, res){
+        // This endpoint is needed for same origin policy (cross domain protection)
+        RED.httpAdmin.get('/devices', function(req, res) {
             self.req = req;
             self.res = res;
 
-            gwcon.apiHandler({'ressource' : 'devices'});
+            gwcon.apiHandler({
+                'ressource': 'devices'
+            });
         });
 
-        gwcon.on('getanswer', function(response){
-            // send back HTTP result of /devices
+        gwcon.on('getanswer', function(response) {
             self.res.send(response.devices);
         });
     }
-    RED.nodes.registerType("enocean gw",EnOceanGW, { 
+    RED.nodes.registerType("enocean gw", EnOceanGW, {
         credentials: {
-             username: {type:"text"},
-             password: {type:"password"}
+            username: {
+                type: "text"
+            },
+            password: {
+                type: "password"
+            }
         }
     });
 
     // Input / Output node
-    function EnOceanOutNode(n){
+    function EnOceanOutNode(n) {
         RED.nodes.createNode(this, n);
         var node = this;
+        this.config = n;
 
         // Retrieve the configuration gw node
         this.gw = RED.nodes.getNode(n.gw);
 
         // check whether a configuration (EnOceanGW()) was set
-        if(!this.gw || this.gw === null){
-            node.status({fill: 'yellow', shape: "ring", text: "No API-Conf set"});
+        if (!this.gw || this.gw === null) {
+            node.status({
+                fill: 'yellow',
+                shape: "ring",
+                text: "No API-Conf set"
+            });
             return;
         }
 
         var gwcon = new APIConnection(this.gw.config, this.gw.credentials);
 
-        var errorFunction = function(e){
+        var errorFunction = function(e) {
             node.error("Error: " + e);
-            node.status({fill: 'red', shape: "ring", text: "message not sent"});
+            node.status({
+                fill: 'red',
+                shape: "ring",
+                text: "message not sent"
+            });
         };
 
         gwcon.on('error', errorFunction);
 
-        gwcon.on('getanswer', function(response){
-            node.send(response);
-            node.status({fill: 'green', shape: "ring", text: "message sent"});
+        gwcon.on('getanswer', function(response) {
+            if (node.config.outputs === 1) {
+                var msg = {
+                    payload: response
+                };
+                node.send(msg);
+            }
+            node.status({
+                fill: 'green',
+                shape: "ring",
+                text: "message sent"
+            });
         });
 
         /*NODE-RED events*/
-        this.on('input', function(msg) {     
+        this.on('input', function(msg) {
 
             // Check type and convert if neccessary
-            if (typeof msg === 'string' || msg instanceof String){
+            if (typeof msg === 'string' || msg instanceof String) {
                 try {
                     msg = JSON.parse(msg);
-                } catch(e) {
+                } catch (e) {
                     node.error('API Input Error: The input has no JSON format');
                     return;
                 }
             }
 
             try {
+                // validate against predefined schema (in ressources/apischema.json)
                 apiSchema.validate(msg.payload);
                 gwcon.apiHandler(msg.payload);
-            } catch(err) {
+            } catch (err) {
                 console.log("Schema check failed: " + err.message);
                 errorFunction(err.message);
             }
         });
 
     }
-    RED.nodes.registerType("enocean out",EnOceanOutNode);
+    RED.nodes.registerType("enocean out", EnOceanOutNode);
 
     // Input node (Streaming)
     function EnOceanInNode(n) {
@@ -98,55 +122,73 @@ module.exports = function(RED) {
         this.gw = RED.nodes.getNode(n.gw);
 
         // check whether a configuration (EnOceanGW()) was set
-        if(!this.gw || this.gw === null){
-            node.status({fill: 'yellow', shape: "ring", text: "No API-Conf set"});
+        if (!this.gw || this.gw === null) {
+            node.status({
+                fill: 'yellow',
+                shape: "ring",
+                text: "No API-Conf set"
+            });
             return;
         }
 
         node.gwcon = new APIConnection(this.gw.config, this.gw.credentials);
         node.config = n;
 
-        node.gwcon.on('getanswer', function(answer){
+        node.gwcon.on('getanswer', function(answer) {
             node.send(answer);
         });
 
-        node.gwcon.on('connected', function(){
-            node.status({fill: 'green', shape: "ring", text: "connected"});
+        node.gwcon.on('connected', function() {
+            node.status({
+                fill: 'green',
+                shape: "ring",
+                text: "connected"
+            });
         });
 
-        node.gwcon.on('states', function(json){
-            if(node.config.statesflag){
+        node.gwcon.on('states', function(json) {
+            if (node.config.statesflag) {
                 var msg = {};
                 msg.payload = json;
                 node.send(msg);
             }
-        });      
+        });
 
-        node.gwcon.on('telegram', function(json){
+        node.gwcon.on('telegram', function(json) {
 
             // if filter set (devices = [deviceId1, deviceId2])
-            if(!node.config.devices || node.config.device === null || node.config.devices.indexOf(json.telegram.deviceId)!==-1){
+            if (!node.config.devices || node.config.device === null || node.config.devices.indexOf(json.telegram.deviceId) !== -1) {
                 var msg = {};
                 msg.payload = json;
                 node.send(msg);
             }
-        }); 
-
-        node.gwcon.on('error', function(e){
-            node.error("Network error: " + e);
-            node.status({fill: 'red', shape: "ring", text: "disconnected"});
         });
 
-        node.status({fill: 'yellow', shape: "ring", text: "connecting"});
+        node.gwcon.on('error', function(e) {
+            node.error("Network error: " + e);
+            node.status({
+                fill: 'red',
+                shape: "ring",
+                text: "disconnected"
+            });
+        });
+
+        node.status({
+            fill: 'yellow',
+            shape: "ring",
+            text: "connecting"
+        });
 
         /*NODE-RED events*/
         this.on('close', function() {
             node.gwcon.closeStream();
         });
 
-        var filter = { direction: n.direction};
+        var filter = {
+            direction: n.direction
+        };
         node.gwcon.startstream(filter);
     }
-    RED.nodes.registerType("enocean in",EnOceanInNode);
+    RED.nodes.registerType("enocean in", EnOceanInNode);
 
 };
